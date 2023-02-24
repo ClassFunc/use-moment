44c70627193088b6bb2fac488e3ddfdcdb2ed471
duongdampa <duong.dam@classfunc.com>
2023-02-24 16:12:39 +0700

    update package

---
+++ index.d.ts
@@ -0,0 +1,16 @@
+import * as mmChecks from "./check";
+import * as mmFormats from "./format";
+import * as mmCalculators from "./calculator";
+import * as mmLocales from "./locale";
+import * as mmExtras from "./extra";
+import * as mmRanges from "./range";
+import * as mmTimezone from "./timezone";
+export {
+  mmChecks,
+  mmCalculators,
+  mmFormats,
+  mmExtras,
+  mmLocales,
+  mmTimezone,
+  mmRanges,
+};
